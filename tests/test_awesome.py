52205bcd80604d89f31e66a4628f74ce60cd2cb4
Aniket Maurya <theaniketmaurya@gmail.com>
2024-12-27 13:17:09 +0000

    update

--- tests/test_awesome.py
+++ tests/test_awesome.py
@@ -3,3 +3,4 @@ from python_project_template.awesome import do_something_awesome
 
 def test_do_something_awesome():
     assert isinstance(do_something_awesome(), str)
+    assert do_something_awesome() == "Congratulations for taking the first step to build your Python project 🎉"
